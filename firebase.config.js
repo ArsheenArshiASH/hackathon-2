eca116d0bb3d13298920a09b559b77900ba4c0cb
ArsheenArshiASH <arsheen.sultan.barak@gmail.com>
2026-01-14 09:16:16 +0500

    starting files

---
+++ firebase.config.js
@@ -0,0 +1,35 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+// auth
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYCNdn47auQDql0v1zufRDctf7X299aDY",
+  authDomain: "mini-hackathon2.firebaseapp.com",
+  projectId: "mini-hackathon2",
+  storageBucket: "mini-hackathon2.firebasestorage.app",
+  messagingSenderId: "901429277858",
+  appId: "1:901429277858:web:4a15f8b7fca2b2043b93fb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {
+  auth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged 
+};
